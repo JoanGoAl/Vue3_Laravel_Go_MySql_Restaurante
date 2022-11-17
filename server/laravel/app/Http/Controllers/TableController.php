b096770a72564e5cdad81a7f64e4dd400cb396f0
JoanGoAl <joangonzalezalbert@gmail.com>
2022-11-17 17:04:18 +0100

    (Laravel) crud por terminar

---
+++ server/laravel/app/Http/Controllers/TableController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+// use Illuminate\Foundation\Bus\DispatchesJobs;
+// use Illuminate\Foundation\Validation\ValidatesRequests;
+// use Illuminate\Routing\Controller as BaseController;
+// Use for request
+use Illuminate\Http\Request;
+
+use App\Models\Table;
+
+class TableController extends Controller
+{
+    public function getTables(Request $request) {
+        return Table::all();
+    }
+
+    public function addTable(Request $request) {
+        return Table::create($request->toArray());
+    }
+
+    public function deleteTable(Request $request, $id) {
+        return Table::destroy($id);
+    }
+
+    public function updateTable(Request $request, $id) {
+        $table = Table::find($id);
+        return $table;
+        // $table->update($request->toArray());
+        // return $table;
+    }
+}
