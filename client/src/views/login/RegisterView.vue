135210e3bb8f6d0318a4b29687eb043140bc1405
JoanGoAl <joangonzalezalbert@gmail.com>
2023-01-10 21:47:53 +0100

    (vue) infoAdicional register

--- client/src/views/login/RegisterView.vue
+++ client/src/views/login/RegisterView.vue
@@ -1,16 +1,87 @@
 <script setup>
+import { ref } from "vue"
+
+const infoUser = ref({
+    email: "",
+    password: "",
+    password2: ""
+})
+
+const infoAdicional = ref({
+    username: "",
+    nombreCompleto: "",
+    direccion: "",
+    telefono: "",
+    dni: ""
+})
+
+const errorEmail = ref(false)
+const errorPassword = ref(["", false])
+
+const seeInfoAdicional = ref(false)
+
+const adicionalError = ref(false)
 
 const handleRegister = (e) => {
     e.preventDefault();
 
+    const regexEmail = /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/
+
+    if (regexEmail.test(infoUser.value.email)) {
+        errorEmail.value = false
+    } else {
+        errorEmail.value = true
+        return
+    }
+    if (infoUser.value.password === "") {
+        console.log("Introduzca una contraseña");
+        errorPassword.value = ["Introduzca una contraseña", true]
+        return
+    }
+    if (infoUser.value.password2 === "") {
+        errorPassword.value = ["Confirme la contraseña", true]
+        return
+    }
+    if (infoUser.value.password !== infoUser.value.password2) errorPassword.value = ["Las contraseñas no coinciden", true]
+    if (infoUser.value.password === infoUser.value.password2) errorPassword.value = ["", false]
+    if (errorEmail.value === false && errorPassword.value[1] === false) {
+        seeInfoAdicional.value = true
+    }
+
 }
+
+const handleRegister2 = (e) => {
+    e.preventDefault();
+
+    let temp = {
+        avatar: `https://www.gravatar.com/avatar/${infoAdicional.value.username}.png`,
+        ...infoAdicional.value
+    }
+
+    let aux = false
+
+    Object.keys(temp).some(item => {
+        if (temp[item] === "") {
+            aux = true
+        }
+    })
+
+    if (aux) {
+
+    }
+
+    console.log(aux);
+
+}
+
+
 
 
 </script>
 
 <template>
     <div class="main-register">
-        <div register>
+        <div register v-if="!seeInfoAdicional">
             <div class="title-container">
                 <h1>REGISTER</h1>
             </div>
@@ -19,17 +90,25 @@ const handleRegister = (e) => {
                     <div formContianer>
                         <div>
                             <label for="email">Email</label>
-                            <input type="text" name="email" id="email">
+                            <input type="text" v-model="infoUser.email" name="email" id="email">
+                        </div>
+                        <div v-if="errorEmail">
+                            <br>
+                            <span class="error">El email no es correcto</span>
                         </div>
                         <br>
                         <div>
                             <label for="password">Contraseña</label>
-                            <input type="password" name="password" id="password">
+                            <input type="password" v-model="infoUser.password" name="password" id="password">
                         </div>
                         <br>
                         <div>
                             <label for="password">Repetir Contraseña</label>
-                            <input type="password" name="password" id="password">
+                            <input type="password" v-model="infoUser.password2" name="password" id="password">
+                        </div>
+                        <div v-if="errorPassword[1]">
+                            <br>
+                            <span class="error">{{ errorPassword[0] }}</span>
                         </div>
                         <div class="container-button">
                             <button @click="handleRegister">Register</button>
@@ -43,10 +122,57 @@ const handleRegister = (e) => {
                 </form>
             </div>
         </div>
+        <div infoAdicional v-if="seeInfoAdicional">
+            <div class="title-container">
+                <h1>Información adicional</h1>
+            </div>
+            <div class="form-container">
+                <form action="">
+                    <div formContianer>
+                        <div>
+                            <label for="username">Username</label>
+                            <input type="text" v-model="infoAdicional.username" name="username" id="username">
+                        </div>
+                        <div>
+                            <label for="nombreCompleto">Nombre completo</label>
+                            <input type="text" v-model="infoAdicional.nombreCompleto" name="nombreCompleto"
+                                id="nombreCompleto">
+                        </div>
+                        <div>
+                            <label for="direccion">Dirección</label>
+                            <input type="text" v-model="infoAdicional.direccion" name="direccion" id="direccion">
+                        </div>
+                        <div>
+                            <label for="telefono">Teléfono</label>
+                            <input type="text" v-model="infoAdicional.telefono" name="telefono" id="telefono">
+                        </div>
+                        <div>
+                            <label for="dni">DNI</label>
+                            <input type="text" v-model="infoAdicional.dni" name="dni" id="dni">
+                        </div>
+                        <div v-if="adicionalError">
+                            <br>
+                            <span class="error">{{ errorPassword[0] }}</span>
+                        </div>
+                        <div class="container-button">
+                            <button @click="handleRegister2">Terminar registro</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
+[infoAdicional] [formContianer]>div {
+    margin-bottom: 15px;
+}
+
+.error {
+    color: red;
+}
+
 .contianer-route {
     margin-top: 15px;
     text-align: right;
@@ -104,6 +230,12 @@ input {
     padding: 20px 0 0 0;
     color: white;
     text-align: center;
+}
+
+[infoAdicional] {
+    background-color: hsla(0, 0%, 0%, 0.7);
+    width: 500px;
+    border-radius: 10px;
 }
 
 [register] {
